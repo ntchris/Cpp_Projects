70fddf4ffe0e7deff1cc7b5465437f63956a2efe
ntchris <ntchris@hotmail.com>
2018-04-01 18:25:54 -0400

    gui wip

--- superdupandl/MainWindow.xaml.cs
+++ superdupandl/MainWindow.xaml.cs
@@ -31,12 +31,22 @@ namespace superdupandl
       public DupanFileListWindow()
       {
             this.DataContext = this;
-            MyBaiduDownloader.testAdd();
+
+
+            MyBaiduDownloader.testRemoteFileListAdd();
+            MyBaiduDownloader.testRemotePathAdd();
+
 
             InitializeComponent();
 
             //Data binding listview and file list
-            listView.ItemsSource = MyBaiduDownloader._fileDownloadList;
+            listView_fileList.ItemsSource = MyBaiduDownloader._remoteFileList;
+
+            
+            listview_pathSelector.ItemsSource = MyBaiduDownloader._remotePathList;
+
+         
+
 
         }
 
@@ -51,5 +61,36 @@ namespace superdupandl
         {
 
         }
+
+        private void buttonDownload_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (checkBoxAll.IsChecked==true)
+            {
+                listView_fileList.SelectAll();
+            }
+
+            int count = listView_fileList.SelectedItems.Count;
+            for (int i = 0; i < count; i++)
+            {
+                superdupandl.MyBaiduDownloader.RemoteFileListItem x = (superdupandl.MyBaiduDownloader.RemoteFileListItem ) listView_fileList.SelectedItems[i];
+                Console.WriteLine(x.filename + " "+ x.size);
+            }
+
+            checkBoxAll.IsChecked = false;
+            listView_fileList.UnselectAll();
+        }
+
+        private void checkBoxAll_Checked(object sender, RoutedEventArgs e)
+        {
+            listView_fileList.SelectAll();
+        }
+
+
+        private void checkBoxAll_unchecked(object sender, RoutedEventArgs e)
+        {
+            listView_fileList.UnselectAll();
+        }
+ 
     }
 }
--- superdupandl/MyBaiduDownloader.cs
+++ superdupandl/MyBaiduDownloader.cs
@@ -8,11 +8,31 @@ using System.Threading.Tasks;
 namespace superdupandl
 {
 
-    
-     
+
+
 
     static class MyBaiduDownloader
     {
+        // for dupan remote file items,  
+        public class RemoteFileListItem
+        {
+            public string filename { get; set; } // file path in baidu net disk
+            public ulong size { get; set; }
+            public DateTime datetime { get; set; }
+            public string remotePath { get; set; } // file path in baidu net disk
+            public bool isfolder { get; set; }
+
+
+            public RemoteFileListItem(string file_name, ulong sizee, DateTime datetimee, string remotepath)
+            {
+                filename = file_name;
+                size = sizee;
+                datetime = datetimee;
+                remotepath = remotePath;
+            }
+
+        };
+
 
         public class ToDownloadFileItem
         {
@@ -21,7 +41,8 @@ namespace superdupandl
             public ulong size { get; set; }
             public DateTime datetime { get; set; }
             public string remotePath { get; set; } // file path in baidu net disk
-             
+            public bool isfolder { get; set; }
+
 
             public ToDownloadFileItem(string file_name, ulong sizee, DateTime datetimee, string remotepath)
             {
@@ -33,12 +54,31 @@ namespace superdupandl
 
         };
 
-        
+
+
+
+
+        public class RemotePathItem
+        {
+            public string foldername { get; set; }
+            public RemotePathItem(string folder)
+            {
+                foldername = folder;
+            }
+
+        };
 
         // allow only one download list
         static public ObservableCollection<ToDownloadFileItem> _fileDownloadList = new ObservableCollection<ToDownloadFileItem>();
-         
-        static public void testAdd()
+
+        static public ObservableCollection<RemoteFileListItem> _remoteFileList = new ObservableCollection<RemoteFileListItem>();
+        
+
+        // could be ie \home\pcgames2018\farcry5 ...  so 3 items in the list, the first item is always home or root or \
+        static public ObservableCollection<RemotePathItem> _remotePathList = new ObservableCollection<RemotePathItem>();
+
+
+        static public void testRemoteFileListAdd()
         {
 
             for (ulong i = 0; i < 10; i++)
@@ -46,10 +86,25 @@ namespace superdupandl
                 string filename = "filename_" + i;
                 string path = "pathdsfggd!!szdfdsfdasfs";
                 ulong size = 100ul * i;
-                ToDownloadFileItem baiduFileItem = new ToDownloadFileItem(filename, size, DateTime.Now, path);
-                _fileDownloadList.Add(baiduFileItem);
+                RemoteFileListItem baiduFileItem = new RemoteFileListItem(filename, size, DateTime.Now, path);
+                _remoteFileList.Add(baiduFileItem);
                 Console.WriteLine(i);
             }
         }
-    };
+
+        static public void testRemotePathAdd()
+        {
+            for (ulong i = 0; i < 5; i++)
+            {
+                RemotePathItem path = new RemotePathItem("remotePath_"  + i);
+
+                _remotePathList.Add(path);
+                Console.WriteLine("!!!!");
+                Console.WriteLine(path.foldername);
+            }
+
+        }
+    }
 }
+
+
