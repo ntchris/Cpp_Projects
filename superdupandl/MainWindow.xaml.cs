00ec7c0bfff8c30ea7fd4c0ae025d2041ac3c667
ntchris <ntchris@hotmail.com>
2018-04-01 12:09:17 -0400

    super dupan downloader GUI bascialy done

---
+++ superdupandl/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+ 
+namespace superdupandl
+{
+
+  
+
+
+   /// <summary>
+   /// Interaction logic for DupanFileListWindow.xaml
+   /// </summary>
+   public partial class DupanFileListWindow : Window
+   {
+
+     
+
+      public DupanFileListWindow()
+      {
+            this.DataContext = this;
+            MyBaiduDownloader.testAdd();
+
+            InitializeComponent();
+
+            //Data binding listview and file list
+            listView.ItemsSource = MyBaiduDownloader._fileDownloadList;
+
+        }
+
+       
+
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+      {
+
+      }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ superdupandl/MyBaiduDownloader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superdupandl
+{
+
+    
+     
+
+    static class MyBaiduDownloader
+    {
+
+        public class ToDownloadFileItem
+        {
+
+            public string filename { get; set; } // file path in baidu net disk
+            public ulong size { get; set; }
+            public DateTime datetime { get; set; }
+            public string remotePath { get; set; } // file path in baidu net disk
+             
+
+            public ToDownloadFileItem(string file_name, ulong sizee, DateTime datetimee, string remotepath)
+            {
+                filename = file_name;
+                size = sizee;
+                datetime = datetimee;
+                remotepath = remotePath;
+            }
+
+        };
+
+        
+
+        // allow only one download list
+        static public ObservableCollection<ToDownloadFileItem> _fileDownloadList = new ObservableCollection<ToDownloadFileItem>();
+         
+        static public void testAdd()
+        {
+
+            for (ulong i = 0; i < 10; i++)
+            {
+                string filename = "filename_" + i;
+                string path = "pathdsfggd!!szdfdsfdasfs";
+                ulong size = 100ul * i;
+                ToDownloadFileItem baiduFileItem = new ToDownloadFileItem(filename, size, DateTime.Now, path);
+                _fileDownloadList.Add(baiduFileItem);
+                Console.WriteLine(i);
+            }
+        }
+    };
+}
